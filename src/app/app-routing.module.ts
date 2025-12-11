ea1fb0286933d1e957c1adc100835245287350b7
UmerCodes <m.umer4713@gmail.com>
2025-12-12 00:35:44 +0500

    Deleted Dist folder. And added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent}
+  {
+    path:'home',
+    component: HomeComponent,
+  },
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full',
+  },
+  {
+    path:'**',
+    redirectTo:'home',
+    pathMatch:'full',
+  }
 ];
 
 @NgModule({
